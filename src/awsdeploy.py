c5dbd53bb05c94916f7c500e5ca7ab7c43d72c6c
RuncibleSpoon <robert.haynes@gmail.com>
2021-07-09 15:08:32 -0700

    into functions

--- src/awsdeploy.py
+++ src/awsdeploy.py
@@ -1,13 +1,40 @@
 #Python for creating an ec2 instance and installing NGINX
 #
 import boto3
+import argpass
+import os
+import time
 
-try:
-  ec2 = boto3.client('ec2')
 
-except Exception as e: print(e)
 
-#This function will describe all the instances
-#with their current state
+def main(vpcid,region,name):
+    try:
+      ec2 = boto3.client('ec2')
+
+    except Exception as e: print(e)
+
+
+
+
+
+
+
 response = ec2.describe_instances()
 print(response)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('vpcid', help='Your AWS VPC')
+    parser.add_argument(
+        '--region',
+        default='us-east-1',
+        help='AWS Region to deploy to.')
+    parser.add_argument(
+        '--name', default='demo-instance', help='New instance name.')
+
+    args = parser.parse_args()
+
+    main(args.vpcid, args.region, args.name)
