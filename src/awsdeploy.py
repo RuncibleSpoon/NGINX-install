b664226df510f506f6bfeca675d5cef3cff7bf97
RuncibleSpoon <robert.haynes@gmail.com>
2021-07-09 19:16:22 -0700

    added stolen code to create security group

--- src/awsdeploy.py
+++ src/awsdeploy.py
@@ -119,7 +119,7 @@ def setup_security_group(group_name, group_description, ec2,ec2Res):
             'IpRanges': [{'CidrIp': '0.0.0.0/0'}]
         }]
         ## removed ssh - not cool
-        sec_group=ec2Res.SecurityGroup(security_group[GroupId])
+        sec_group=ec2Res.SecurityGroup(sgID)
         sec_group.authorize_ingress(IpPermissions=ip_permissions)
         print("Set inbound rules for %s to allow all inbound HTTP and HTTPS "
                     "but only %s for SSH.", security_group.id, ssh_ingress_ip)
