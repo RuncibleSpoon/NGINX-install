856b260b8ddb3959481ac367711231d0dcb15379
RuncibleSpoon <robert.haynes@gmail.com>
2021-07-09 18:50:21 -0700

    added stolen code to create security group

--- src/awsdeploy.py
+++ src/awsdeploy.py
@@ -99,7 +99,7 @@ def setup_security_group(group_name, group_description, ec2,ec2Res):
     ## removed the code to find the default VPC since I want it specified
 
     try:
-        security_group = ec2cl.create_security_group(
+        security_group = ec2.create_security_group(
             GroupName=group_name, Description=group_description)
 #         logger.info(
         print("Created security group %s in VPC %s.", group_name, default_vpc.id)
