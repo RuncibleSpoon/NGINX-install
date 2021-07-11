f00ab813e9c9d67f2652e6a2db805c96d8243cc1
RuncibleSpoon <robert.haynes@gmail.com>
2021-07-11 13:18:47 -0700

    added some more parameters

--- src/cftdeploy.py
+++ src/cftdeploy.py
@@ -90,7 +90,7 @@ def main(vpcid,region,name,keypair):
         # retrieve bucket object - could this be more elegant?
         bucket = s3res.Bucket(s3BucketName)
         contentUrl = "https://s3.amazonaws.com/" + s3BucketName + "/index.html
-        configUrl = "https://s3.amazonaws.com/" + s3BucketName + "/nginx.conf
+        configUrl = "https://s3.amazonaws.com/" + s3BucketName + "/nginx.conf"
         templateUrl = "https://s3.amazonaws.com/" + s3BucketName + "/nginx.cft"
         print(templateUrl)
         print('S3bucket Created')
